03a993aa93d50222264adc7fd19b21a564f654f7
Berton Earnshaw <berton.earnshaw@gmail.com>
2014-03-02 22:36:34 -0700

    Add ability to read from DNS file
    When sql statement looks like '%sql [section] <sql_statement>',
    a sqlalchemy connection string is created from the corresponding section
    in the DNS file specified in the configuration dns_filename.

--- src/sql/parse.py
+++ src/sql/parse.py
@@ -1,10 +1,22 @@
-import re
+from ConfigParser import ConfigParser
+from sqlalchemy.engine.url import URL
 
-def parse(cell):
-    parts = cell.split(None, 1)
+
+def parse(cell, config):
+    parts = [part.strip() for part in cell.split(None, 1)]
     if not parts:
         return {'connection': '', 'sql': ''}
-    if '@' in parts[0] or '://' in parts[0]:
+    if parts[0].startswith('[') and parts[0].endswith(']'):
+        parser = ConfigParser()
+        parser.read(config.dsn_filename)
+        section = parts[0].lstrip('[').rstrip(']')
+        connection = str(URL(drivername=parser.get(section, 'drivername'),
+                             username=parser.get(section, 'username'),
+                             password=parser.get(section, 'password'),
+                             host=parser.get(section, 'host'),
+                             database=parser.get(section, 'database')))
+        sql = parts[1] if len(parts) > 1 else ''
+    elif '@' in parts[0] or '://' in parts[0]:
         connection = parts[0]
         if len(parts) > 1:
             sql = parts[1]
