c6f0ee08475974ffb92ff73b393a4b757e2535cb
wilsaj <wilson.andrew.j+github@gmail.com>
2015-01-10 14:38:56 -0600

    make dsn_filename config help string a single line so it prints nicer

--- src/sql/magic.py
+++ src/sql/magic.py
@@ -27,10 +27,10 @@ class SqlMagic(Magics, Configurable):
     displaylimit = Int(0, config=True, help="Automatically limit the number of rows displayed (full result set is still stored)")
     autopandas = Bool(False, config=True, help="Return Pandas DataFrames instead of regular result sets")
     feedback = Bool(True, config=True, help="Print number of rows affected by DML")
-    dsn_filename = Unicode('odbc.ini', config=True, help="Path to DSN file. \
-                           When the first argument is of the form [section], \
-                           a sqlalchemy connection string is formed from the \
-                           matching section in the DSN file.")
+    dsn_filename = Unicode('odbc.ini', config=True, help="Path to DSN file. "
+                           "When the first argument is of the form [section], "
+                           "a sqlalchemy connection string is formed from the "
+                           "matching section in the DSN file.")
 
     def __init__(self, shell):
         Configurable.__init__(self, config=shell.config)
