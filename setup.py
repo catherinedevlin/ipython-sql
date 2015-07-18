7eb354fd2e2e963db65cc81985c000b70445fb1c
Catherine Devlin <catherine.devlin@gmail.com>
2015-07-18 19:03:35 -0400

    Advance version to 0.3.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.3.6'
+version = '0.3.7'
 
 install_requires = [
     'prettytable',
