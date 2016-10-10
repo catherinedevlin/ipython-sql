0bd476563effeeddaa2ebd3f56fbf485ad90f513
Catherine <catherine.devlin@gmail.com>
2016-10-10 17:22:28 -0400

    Begin v 0.3.9, add test for autopandas

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.3.8'
+version = '0.3.9'
 
 install_requires = [
     'prettytable',
