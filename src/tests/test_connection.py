f8c9d1b66bebf3ba36cde3bfcf4aed58c9c23802
Eduardo Blancas <github@blancas.io>
2022-12-30 19:21:36 -0600

    mocking psycopg2

--- src/tests/test_connection.py
+++ src/tests/test_connection.py
@@ -1,3 +1,4 @@
+import sys
 from unittest.mock import Mock
 
 from sqlalchemy.engine import Engine
@@ -6,7 +7,9 @@ from sql.connection import Connection
 
 
 def test_password_isnt_displayed(monkeypatch):
+    monkeypatch.setitem(sys.modules, "psycopg2", Mock())
     monkeypatch.setattr(Engine, "connect", Mock())
+
     Connection.from_connect_str("postgresql://user:topsecret@somedomain.com/db")
 
     assert "topsecret" not in Connection.connection_list()
