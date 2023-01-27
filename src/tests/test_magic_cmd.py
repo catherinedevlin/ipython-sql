5f231c0ad481f733c7d0c298985c1dcb1cf49439
Eduardo Blancas <github@blancas.io>
2023-01-27 12:50:41 -0300

    test updates

--- src/tests/test_magic_cmd.py
+++ src/tests/test_magic_cmd.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 import pytest
 from IPython.core.error import UsageError
 
@@ -31,7 +33,6 @@ def test_tables(ip):
     assert "test" in out
 
 
-# try with schema
 def test_columns(ip):
     out = ip.run_cell("%sqlcmd columns -t author").result._repr_html_()
     assert "first_name" in out
@@ -39,6 +40,18 @@ def test_columns(ip):
     assert "year_of_death" in out
 
 
-# MISSING TESTS
-# # %sqlcmd x --help
-# %sqlcmd columns --help
+def test_columns_with_schema(ip, tmp_empty):
+    with sqlite3.connect("my.db") as conn:
+        conn.execute("CREATE TABLE numbers (some_number FLOAT)")
+
+    ip.run_cell(
+        """%%sql
+ATTACH DATABASE 'my.db' AS some_schema
+"""
+    )
+
+    out = ip.run_cell(
+        "%sqlcmd columns --table numbers --schema some_schema"
+    ).result._repr_html_()
+
+    assert "some_number" in out
