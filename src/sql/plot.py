e81e0ae27ca29642768c98a2edbb16168b09392d
Tony Kuo <123580782+tonykploomber@users.noreply.github.com>
2023-02-24 19:10:30 -0500

    Fix: doc build fail cause _get_curr_connection_info (#165)
    * Fix: Plot.py
    
    * Fix: Plot.py

--- src/sql/plot.py
+++ src/sql/plot.py
@@ -244,9 +244,10 @@ def boxplot(payload, table, column, *, orient="v", with_=None, conn=None):
     if not conn:
         conn = sql.connection.Connection.current.session
 
-    payload[
-        "connection_info"
-    ] = sql.connection.Connection.current._get_curr_connection_info()
+    if sql.connection.Connection.current:
+        payload[
+            "connection_info"
+        ] = sql.connection.Connection.current._get_curr_connection_info()
 
     ax = plt.gca()
     vert = orient == "v"
@@ -328,9 +329,10 @@ def histogram(payload, table, column, bins, with_=None, conn=None):
     .. plot:: ../examples/plot_histogram_many.py
     """
     ax = plt.gca()
-    payload[
-        "connection_info"
-    ] = sql.connection.Connection.current._get_curr_connection_info()
+    if sql.connection.Connection.current:
+        payload[
+            "connection_info"
+        ] = sql.connection.Connection.current._get_curr_connection_info()
     if isinstance(column, str):
         bin_, height = _histogram(table, column, bins, with_=with_, conn=conn)
         ax.bar(bin_, height, align="center", width=bin_[-1] - bin_[-2])
